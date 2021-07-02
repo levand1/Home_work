1f209df66918365b4cbc68e15192024393f96dca
unknown <andreilev1881@gmail.com>
2021-07-02 12:31:32 +0300

    script file for hw1.txt

---
+++ script.sh
@@ -0,0 +1,16 @@
+
+#! /bin/bash
+
+cd /name_folder
+
+mkdir name_folder1 name_folder2 name_folder3
+
+cd name_folder1
+
+touch name1.txt name2.txt name3.txt name4.json name5.json
+
+mkdir folder1 folder2 folder3
+
+ls
+
+mv name1.txt name4.json /name_folder/name_folder2
